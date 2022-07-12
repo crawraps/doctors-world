8becabd8f94389516fcc7d6051cba92f0599be99
crawraps <crawraps@gmail.com>
2022-07-12 03:41:44 +0300

    Add variants block

---
+++ components/Variant.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react'
+import styles from '../styles/Variant.module.css'
+import Image from 'next/image'
+import { usePatient } from '../context'
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+  type: 'adult' | 'kid'
+  title: string
+  image: string
+  cards: string[]
+  className?: string
+}
+
+function Variant({ type, title, image, cards, className = '', ...props }: Props): JSX.Element {
+  const patient = usePatient().state.patient
+  const [isOpen, setIsOpen] = React.useState<boolean>(false)
+
+  // Calculate if description is open in mobile version
+  React.useEffect(() => setIsOpen(patient === type), [type, patient])
+
+  return (
+    <div className={styles.container + ' ' + className} {...props}>
+      <h3>{title}</h3>
+      <div className={styles.imageContainer}>
+        <Image src={image} alt={title} width={300} height={300} />
+      </div>
+      <div className={styles.descriptionContainer}>
+        {cards.map((text, index) => (
+          <div className={styles.card} key={`${type}-card-${index}`}>
+            <p>{text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default React.memo(Variant)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { AdultDescriptions, KidDescriptions } from '../assets/texts/Descriptions'
 import Container from '../components/Container'
+import Variant from '../components/Variant'
 import { PatientProvider } from '../context'
 import styles from '../styles/Home.module.css'
 
@@ -14,9 +16,19 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Container className={styles.container}>
+      <Container className={styles.container} padding>
         <h1>А вдруг СМА?</h1>
         <h2>Выберете, кто Ваш пациент:</h2>
+
+        <div className={styles.variantsContainer}>
+          <Variant
+            type='adult'
+            title='Взрослый'
+            image={require('../assets/images/wooman.jpg')}
+            cards={AdultDescriptions}
+          />
+          <Variant type='kid' title='Ребенок' image={require('../assets/images/kid.jpg')} cards={KidDescriptions} />
+        </div>
       </Container>
     </PatientProvider>
   )
